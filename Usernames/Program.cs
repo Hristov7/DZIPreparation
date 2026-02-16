fc2e3dee6e4da10863dd4715ede8cc1248bb3983
Hristov7 <danielantonov07@gmail.com>
2026-02-16 17:37:51 +0200

    ValidUserNames

---
+++ Usernames/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> userNames = ValidUsernames(input);
+            Console.WriteLine(string.Join('\n', userNames));
+        }
+        public static List<string> ValidUsernames(string[] input)
+        {
+            List<string> validUserNames = new();
+            foreach (string item in input)
+            {
+                bool flag = false;
+                if (item.Length <= 3 || item.Length >= 16)
+                {
+                    continue;
+                }
+
+                foreach (char symbol in item)
+                {
+                    if (!char.IsLetter(symbol) && !char.IsDigit(symbol) && symbol != '_' && symbol != '-') break;
+                    else flag = true;
+                }
+                if(flag) validUserNames.Add(item);
+            }
+            return validUserNames;
+        }
+    }
+}
+// sh, too_long_username, !lleg@l ch@rs, jeffbutt
+
+//Jeff, john45, ab, cd, peter-ivanov, @smith
