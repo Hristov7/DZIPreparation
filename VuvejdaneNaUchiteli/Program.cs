f3d6b54998b983086ea659c8ed25045be454ad82
Hristov7 <danielantonov07@gmail.com>
2026-02-16 22:44:00 +0200

    Programka za vuvejdane na uchiteli, 1 klas.

---
+++ VuvejdaneNaUchiteli/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace VuvejdaneNaUchiteli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            List<Teacher> teachers = new List<Teacher>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine()!;
+                string lessonType = Console.ReadLine()!;
+                int yearsOfPractice = int.Parse(Console.ReadLine()!);
+                teachers.Add(new Teacher(name, lessonType, yearsOfPractice));
+            }
+
+            Console.WriteLine($"{string.Join('\n', teachers.OrderBy(x => x.Name))}");
+
+            double averageExperience = teachers.Average(x => x.YearsOfPractice);
+            Console.WriteLine($"Sredniqt staj na uchitelite e: {averageExperience:f2} godini.");
+
+            int maxYearOfExperience = teachers.Max(t => t.YearsOfPractice);
+            List<Teacher> mostExperiencedTeachers = teachers.Where(t => t.YearsOfPractice == maxYearOfExperience).ToList();
+            Console.WriteLine($"{string.Join('\n', mostExperiencedTeachers)}");
+
+            IEnumerable<IGrouping<string, Teacher>> groupsOfTeachers = teachers.GroupBy(t => t.LessonType);
+
+            foreach (IGrouping<string, Teacher> group in groupsOfTeachers)
+            {
+                Console.WriteLine($"{group.Key} - {group.Count()}");
+            }
+
+            IEnumerable<IGrouping<string, Teacher>> groupedTeachersByExperience = teachers.GroupBy(t =>
+            {
+                if (t.YearsOfPractice < 5) return $"Начинаещ учител";
+                else if (t.YearsOfPractice >= 5 && t.YearsOfPractice <= 25) return $"Опитен учител";
+                else return $"Ветеран в образованието";
+                
+            });
+
+            foreach (IGrouping<string, Teacher> group in groupedTeachersByExperience)
+            {
+                Console.WriteLine($"{group.Key}:");
+                foreach (Teacher teacher in group)
+                {
+                    Console.WriteLine($"{teacher.ToString()}");
+                }
+            }
+        }
+    }
+}
+/*
+3
+Иван Иванов
+Математика
+15
+Галя Маринова
+История
+5
+Мария Георгиева
+Математика
+30
+
+*/
---
+++ VuvejdaneNaUchiteli/Teacher.cs
@@ -0,0 +1,36 @@
+﻿namespace VuvejdaneNaUchiteli
+{
+    public class Teacher
+    {
+        private string _name;
+        private string _lessonType;
+        private int _yearsOfPractice;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string LessonType
+        {
+            get { return _lessonType; }
+            set { _lessonType = value; }
+        }
+        public int YearsOfPractice
+        {
+            get { return _yearsOfPractice; }
+            set { _yearsOfPractice = value; }
+        }
+
+        public Teacher(string name, string lessonType, int yearsOfPractice)
+        {
+            Name = name;
+            LessonType = lessonType;
+            YearsOfPractice = yearsOfPractice;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} e uchitel po {this.LessonType} ot {this.YearsOfPractice} godini ";
+        }
+    }
+}
